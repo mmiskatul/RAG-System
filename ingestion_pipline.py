730004999edbeca2d2854562a24a1e28c273ff95
Md Miskatul Masabi <masabimiskat@gmail.com>
2026-01-14 11:19:56 +0600

    add load_document function

--- ingestion_pipline.py
+++ ingestion_pipline.py
@@ -4,7 +4,7 @@ from langchain_text_splitters import CharacterTextSplitter
 from langchain_chroma import Chroma 
 from sentence_transformers import SentenceTransformer
 
-model = SentenceTransformer("BAAI/bge-m3")
+# model = SentenceTransformer("BAAI/bge-m3")
 
 # sentences = [
 #     "That is a happy person",
@@ -18,8 +18,40 @@ model = SentenceTransformer("BAAI/bge-m3")
 # print(similarities.shape)
 # # [4, 4]
 
+def load_documents(docs_path="docs"):
+    """Load the text files from the docs directory"""
+    print(f"Loading documents from {docs_path}...")
+    
+    #Check if docs directory exists 
+    if not os.path.exists(docs_path):
+        raise FileNotFoundError(f"The directory {docs_path} does not exits . Please create it and add your company files")
+    
+    
+    #load all .txt file from the docs directory 
+    loader =DirectoryLoader(
+        path=docs_path,
+        glob="*.txt",
+        loader_cls=TextLoader
+    )
+    documents =loader.load()
+    
+    if len(documents) == 0:
+        raise FileNotFoundError(f"No .txt files found in {docs_path}. Please add your company documents.")
+    
+    for i,doc in enumerate(documents[:2]):
+        print(f"\nDocuments {i+1}")
+        print(f" Source: {doc.metadata['source']}")
+        print(f" Content length: {len(doc.page_content)} characters")
+        print(f" Content preview: {doc.page_content[:100]}...")
+        print(f" metadata: {doc.metadata}")
+    
+    return documents 
+ 
 def main():
     print("main function")
+    
+    #load the file 
+    documents =load_documents(docs_path="docs")
 
 if __name__ == "__main__" :
     main()
